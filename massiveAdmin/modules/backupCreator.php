a92d455ab87fb8d6ce2c6970d9af519a29dac803
risingisland <risingisland@yahoo.com>
2024-07-01 16:32:20 +0100

    5.0.8
    W3 css style

--- massiveAdmin/modules/backupCreator.php
+++ massiveAdmin/modules/backupCreator.php
@@ -1,49 +1,31 @@
-<style>
-    .backuplist {
-        margin: 10px 0 !important;
-        padding: 0 !important;
-        list-style-type: none;
-    }
-    .backuplist li {
-        padding: 10px;
-        border-bottom: solid 1px #ddd;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-    }
-    .backuplist li:nth-child(2n) {
-        background: #fafafa;
-    }
-    .delbackupbtn {
-        cursor: pointer;
-        background: var(--main-color);
-        border: none;
-    }
-    .delbackupbtn svg {
-        stroke: #fff !important;
-    }
-</style>
-
 <link rel="stylesheet" href="<?php global $SITEURL; echo $SITEURL; ?>plugins/massiveAdmin/css/w3.css">
+<style>.w3-ul li:nth-child(odd) {background: #F3F3F3;}</style>
 
 <div class="w3-parent w3-container"><!-- Start Plug -->
 
 <h3>Backup creator</h3>
+<hr>
+<div class="w3-parent w3-container">
+	<form action="#" method="post">
+		<select class="w3-select w3-padding w3-margin-bottom w3-border w3-round" name="folder" id="" xstyle="width:100%;padding:10px;margin-bottom:10px;background:#fff;border:solid 1px #ddd;">
+			<option value="<?php echo GSPLUGINPATH; ?>">Plugins folder</option>
+			<option value="<?php echo GSADMINPATH; ?>">Admin folder</option>
+			<option value="<?php echo GSDATAPATH; ?>">Data folder</option>
+			<option value="<?php echo GSTHEMESPATH; ?>">Themes folder</option>
+			<option value="<?php echo GSDATAUPLOADPATH; ?>">Uploads folder</option>
+		</select>
+		
+		<div class="w3-margin-top w3-center">
+			<button class="w3-btn w3-large w3-round-large w3-green" style="width:33.3%" type="submit" name="backupcreate"><?php echo i18n_r('massiveAdmin/CREATEBACKUP'); ?></button>
+		</div>
+	</form>
+</div>
 
-<form action="#" method="post">
-    <select name="folder" id="" style="width:100%;padding:10px;margin-bottom:10px;background:#fff;border:solid 1px #ddd;">
-        <option value="<?php echo GSPLUGINPATH; ?>">Plugins folder</option>
-        <option value="<?php echo GSADMINPATH; ?>">Admin folder</option>
-        <option value="<?php echo GSDATAPATH; ?>">Data folder</option>
-        <option value="<?php echo GSTHEMESPATH; ?>">Themes folder</option>
-        <option value="<?php echo GSDATAUPLOADPATH; ?>">Uploads folder</option>
-    </select>
-	
-    <button class="button" name="backupcreate" style="padding:10px 15px"><?php echo i18n_r('massiveAdmin/CREATEBACKUP'); ?></button>
-</form>
-
+<div class="w3-container w3-margin-top">
+<hr>
 <?php
-	echo '<ul class="backuplist">';
+	echo '
+	<ul class="w3-ul w3-hoverable w3-margin-top">';
 
 	foreach (glob(GSBACKUPSPATH . 'backupCreator/*.*') as $zip) {
 		global $SITEURL;
@@ -53,13 +35,27 @@
 		$url = GSBACKUPSPATH . 'backupCreator/' . pathinfo($zip)['basename'];
 		$name =   pathinfo($zip)['filename'] . '.zip';
 
-		echo "<li><a href='$domainurl' download>$name</a> <form method='post'><form method='post'><input type='hidden' name='delbackup' value='$url'><button class='delbackupbtn' type='submit'><svg width='20px' height='20px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
-		<path d='M18 6L17.1991 18.0129C17.129 19.065 17.0939 19.5911 16.8667 19.99C16.6666 20.3412 16.3648 20.6235 16.0011 20.7998C15.588 21 15.0607 21 14.0062 21H9.99377C8.93927 21 8.41202 21 7.99889 20.7998C7.63517 20.6235 7.33339 20.3412 7.13332 19.99C6.90607 19.5911 6.871 19.065 6.80086 18.0129L6 6M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/>
-		</svg></button></form>";
+		echo '
+		<li>
+			<div class="w3-row-padding">
+				<div class="w3-half">
+					<a href="'.$domainurl.'" download>'. $name.'</a> 
+				</div>
+				<div class="w3-half w3-right-align" style="width:46%">
+					<form method="post"><input type="hidden" name="delbackup" value="'.$url.'">
+						<button class="delbackupbtn w3-bar-item w3-btn w3-red w3-round w3-right" style="padding:0 5px; xborder:black solid 1px" type="submit">
+							<svg xmlns="http://www.w3.org/2000/svg" width="1.3em" height="1.3em" viewBox="0 0 24 24" id="trash"><path fill="#fff" d="M20,6H16V5a3,3,0,0,0-3-3H11A3,3,0,0,0,8,5V6H4A1,1,0,0,0,4,8H5V19a3,3,0,0,0,3,3h8a3,3,0,0,0,3-3V8h1a1,1,0,0,0,0-2ZM10,5a1,1,0,0,1,1-1h2a1,1,0,0,1,1,1V6H10Zm7,14a1,1,0,0,1-1,1H8a1,1,0,0,1-1-1V8H17Z"></path></svg>
+						</button>
+					</form>
+				</div>
+			</div>
+		</li>';
 	};
 
-	echo '</ul>';
+	echo '
+	</ul>';
 ?>
+</div>
 
 <?php
 	global $MA;
--- massiveAdmin/modules/snippet.php
+++ massiveAdmin/modules/snippet.php
@@ -45,7 +45,7 @@
 						</div>
 						
 						<div class="w3-half w3-right-align" style="width:46%">
-							<button xstyle="border-radius:5px; position:absolute; top:5px; right:10px; background:red; color:#fff; border:none;" onclick="event.preventDefault();closeThisSnippet(this)"> ✕ </button>
+							<button class="w3-bar-item w3-btn w3-red w3-round w3-right"  style="padding:0 5px; border:black solid 1px" onclick="event.preventDefault();closeThisSnippet(this)"><svg xmlns="http://www.w3.org/2000/svg" width="1.3em" height="1.3em" viewBox="0 0 24 24" id="trash"><path fill="#fff" d="M20,6H16V5a3,3,0,0,0-3-3H11A3,3,0,0,0,8,5V6H4A1,1,0,0,0,4,8H5V19a3,3,0,0,0,3,3h8a3,3,0,0,0,3-3V8h1a1,1,0,0,0,0-2ZM10,5a1,1,0,0,1,1-1h2a1,1,0,0,1,1,1V6H10Zm7,14a1,1,0,0,1-1,1H8a1,1,0,0,1-1-1V8H17Z"></path></svg></button></button>
 						</div>
 					</div>
 					
